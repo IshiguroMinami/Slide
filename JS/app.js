a1f8d7607a4eecb73360ceb401fd0e84aa499e0f
石黒みなみ <ishigurominami142@gmail.com>
2025-04-20 20:25:42 +0900

    first commit

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.carousel').flickity({
+    cellAlign: 'left',
+    contain: true,
+    pageDots: true,
+    prevNextButtons: true,
+    wrapAround: true,
+    imagesLoaded: true,
+    setGallerySize: true,
+    autoPlay: 5000
+  });
+});
